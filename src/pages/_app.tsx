0adfa4f4a0534f3d314f2273ff12367f146b04eb
Andra <yeromrenandra07@gmail.com>
2024-04-02 16:59:46 +0700

    style: Adding favicon.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
         />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Component {...pageProps} />
