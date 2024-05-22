9001fda8ba19e02969094ff832311962ec826f4a
Andranovia <yeromrenandra07@gmail.com>
2024-05-22 11:08:28 +0700

    style: Fixing hero section responsive

--- src/components/hero/Index.tsx
+++ src/components/hero/Index.tsx
@@ -12,7 +12,7 @@ function Hero() {
 
   return (
     <div className={bebasNeue.className}> 
-      <div className="bg-primary overflow-hidden lg:overflow-auto lg:mt-0  rounded-xl relative flex justify-start bg-dot-white/[0.3] lg:justify-center  items-center    lg:pt-[4rem] font-bold  ">
+      <div className="bg-primary overflow-hidden lg:overflow-visible  lg:mt-0  rounded-xl relative flex justify-start bg-dot-white/[0.3] lg:justify-center  items-center    lg:pt-[4rem] font-bold  ">
       <div className="bg-primary hidden absolute pointer-events-none inset-0 z-10 lg:flex items-center justify-center rounded-xl   [mask-image:radial-gradient(ellipse_at_center,transparent_70%,black)]"></div>
         <HeroText/>
         <div className="absolute pointer-events-none -bottom-[14%]  lg:-bottom-[30%] -right-[10%]">
